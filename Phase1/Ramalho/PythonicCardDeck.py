9747a06e9545257da5221ab04a2555974930e7d1
Sampatankar <50807100+Sampatankar@users.noreply.github.com>
2025-12-24 11:48:58 +0000

    Completed the Pythonic Card Deck initial special methods intro

--- Phase1/Ramalho/PythonicCardDeck.py
+++ Phase1/Ramalho/PythonicCardDeck.py
@@ -31,5 +31,25 @@ print(f"The first card is:",deck[0],"and the last card is:",deck[-1])
 from random import choice
 print(choice(deck), choice(deck), choice(deck))
 
+# Look at the top three cards from a new deck and pick just the aces by starting on index 12 and skipping 13 cards at a time:
+print(f"The top three cards of a brand new deck:",deck[:3])
+print(f"Picking aces by starting on index 12 and skipping 13 cards at a time:",deck[12::13])
 
+# Forward iteration:
+for card in deck:
+    print(f"The forward iteration:", card)
+
+# Reversed iteration:
+for card in reversed(deck):
+    print(f"The reversed deck:", card)
+
+# Ranking and sorting the cards:
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+def spades_high(card) -> int:
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+for card in sorted(deck, key=spades_high):
+    print(f"The sorted deck:",card)
 
