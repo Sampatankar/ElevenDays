68815242c832a25f27b018363d42068c15c0c947
Sampatankar <50807100+Sampatankar@users.noreply.github.com>
2025-12-28 17:16:49 +0000

    Last of the testing items from Slatkin

---
+++ Phase1/Slatkin/utils.py
@@ -0,0 +1,10 @@
+# from Item 76 - verifying related behaviours in testcase subclasses
+
+def to_str(data):
+    if isinstance(data, str):
+        return data
+    elif isinstance(data, bytes):
+        return data.decode('utf-8')
+    else:
+        raise TypeError('Must supply str or bytes, '
+                        'found: %r' % data)
