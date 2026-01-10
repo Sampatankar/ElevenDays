37d2c347429207f890012e656f1676fd30cb46fb
Sampatankar <50807100+Sampatankar@users.noreply.github.com>
2026-01-10 21:52:35 +0000

    Intialise the example end-to-end ML pipeline in Geron - phase 2

---
+++ Phase2/Geron/example_ml_pipline.py
@@ -0,0 +1,18 @@
+"""
+This is the initial file for the Geron, ch2 end-to-end ML project.
+
+This project will initialise the following skills:
+1. Look at the big picture to frame the problem
+2. Get the data
+3. Explore and visualise the data to gain insights
+4. Prepare the data for machine learning algorithms
+5. Select a model and train it by shortlisting the best models
+6. Fine-tune your model
+7. Present your solutions
+8. Launch, monitor and maintain your system
+
+"""
+
+
+
+
