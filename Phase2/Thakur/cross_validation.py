3f7e961e43c3f546fb82e5357ce55d7819914bc3
Sampatankar <50807100+Sampatankar@users.noreply.github.com>
2026-01-14 12:27:54 +0000

    cross-evaluation section completed

--- Phase2/Thakur/cross_validation.py
+++ Phase2/Thakur/cross_validation.py
@@ -231,11 +231,51 @@ splitting the data into k-equal parts using stratified k-fold cross-validation f
 Below is the use of k-fold cross-validation for a regression problem, rather than the classification problems above.
 """
 
+# stratified k-fold for regression:
+import numpy as np
+import pandas as pd
+
+from sklearn import datasets
+from sklearn import model_selection
+
+def create_folds(data):
+    # we create a new column called kfold and fill it with -1
+    data = data.sample(frac=1).reset_index(drop=True)
+
+    # calculate the number of bins by Sturge's rule (taking the floor value but also you can round it):
+    num_bins = int(np.floor(1 + np.log2(len(data))))
+
+    # bin targets:
+    data.loc[:, "bins"] = pd.cut(
+        data["target"], bins=num_bins, labels=False
+    )
+
+    # initiate the kfold class from model_selection module:
+    kf = model_selection.StratifiedKFold(n_splits=4)
+
+    # fill the new kfold column - note we now use bins, not targets:
+    for f, (t_, v_) in enumerate(kf.split(X=data, y=data.bins.values)):
+        data.loc[v_, 'kfold'] = f
+
+    # drop the bins columns:
+    data = data.drop("bins", axis=1)
+    # return dataframe with folds:
+    return data
 
 
+if __name__ == "__main__":
+    # we create a sample dataset with 15000 samples and 100 features and 1 target:
+    X, y = datasets.make_regression(
+        n_samples = 15000, n_features = 100, n_targets = 1
+    )
 
+    # create a dataframe out of our numpy arrays:
+    df = pd.DataFrame(
+        X,
+        columns = [f"f_{i}" for i in range(X.shape[1])]
+    )
+    df.loc[:, "target"] = y
 
-
-
-
+    # create folds:
+    df = create_folds(df)
 
