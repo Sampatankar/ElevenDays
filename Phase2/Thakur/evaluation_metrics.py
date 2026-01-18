9c2e3f2d40394be5b8668cf52f8f7a1a0ae368f6
Sampatankar <50807100+Sampatankar@users.noreply.github.com>
2026-01-18 19:07:59 +0000

    implementing fpr vs tpr and plotting it

--- Phase2/Thakur/evaluation_metrics.py
+++ Phase2/Thakur/evaluation_metrics.py
@@ -242,11 +242,10 @@ def f1(y_true, y_pred):
     score = (2 * p * r) / (p + r)
     return score
 
-y_true = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0]
-y_pred = [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0]
-
-
 # uncomment the print statement for this basic accuracy score:
+# y_true = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+# y_pred = [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+
 # print(f1(y_true, y_pred))
 
 # # implement f1 from sklearn:
@@ -285,6 +284,45 @@ def fpr(y_true, y_pred):
     tn = true_negative(y_true, y_pred)
     return fp / (tn + fp)
 
+
+# calculating TPR and FPR for varying thresholds
+tpr_list = []
+fpr_list = []
+
+# actual targets:
+y_true = [0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1]
+
+# predicted probabilities of a sample being 1:
+y_pred = [
+    0.1, 0.3, 0.2, 0.6, 0.8, 0.05, 0.9, 0.5, 0.3, 0.66, 0.3, 0.2, 0.85, 0.15, 0.99
+]
+
+# handmade thresholds:
+thresholds = [
+    0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.99, 1.0
+]
+
+# loop over all thresholds:
+for threshold in thresholds:
+    # calculate predictions for a given threshold:
+    temp_pred = [1 if x >= threshold else 0 for x in y_pred]
+    # calculate tpr:
+    temp_tpr = tpr(y_true, temp_pred)
+    # calculate fpr:
+    temp_fpr = fpr(y_true, temp_pred)
+    # append for tpr and fpr to lists
+    tpr_list.append(temp_tpr)
+    fpr_list.append(temp_fpr)
+
+
+plt.figure(figsize=(7, 7))
+plt.fill_between(fpr_list, tpr_list, alpha=0.4)
+plt.plot(fpr_list, tpr_list, lw=3)
+plt.xlim(0, 1.0)
+plt.ylim(0, 1.0)
+plt.xlabel('FPR', fontsize=15)
+plt.ylabel('TPR', fontsize=15)
+plt.show()
 
 
 
