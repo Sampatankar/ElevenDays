289c57da8b8e4bd6d5ca64d1438098531349721b
Sampatankar <50807100+Sampatankar@users.noreply.github.com>
2026-01-18 17:39:26 +0000

    implemented a p-r curve

--- Phase2/Thakur/evaluation_metrics.py
+++ Phase2/Thakur/evaluation_metrics.py
@@ -1,3 +1,7 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 """
 This is the initial file relating to the Evaluation Metrics section from pg.30 in Thakur.
 
@@ -184,6 +188,41 @@ def recall(y_true, y_pred):
 
 # uncomment the print statement for this basic accuracy score:
 # print(recall(l1, l2))
+
+
+# Implementing the Precision-Recall curve:
+"""
+y_true are our target values.  y_pred is the probability values for a sample being assigned a value of 1.  So, now, we look at probabilities in prediction instead of of the predicted value (which is most of the time calculated with a threshold at 0.5)
+"""
+y_true = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+y_pred = [
+    0.02638412, 0.11114267, 0.31620708, 0.0490937, 0.0191491, 0.17554844, 0.15952202, 0.03819563, 0.11639273, 0.079377, 0.08584789, 0.39095342, 0.27259048, 0.03447096, 0.04644807, 0.03543574, 0.18521942, 0.05934905, 0.61977213, 0.33056815
+]
+
+
+precisions = []
+recalls = []
+
+thresholds = [
+    0.0490937, 0.05934905, 0.079377, 0.08584789, 0.11114267, 0.11639273, 0.15952202, 0.17554844, 0.18521942, 0.27259048, 0.31620708, 0.33056815, 0.39095342, 0.61977213
+]
+
+# for every threshold, calculate predictions in binary and append calculated precisions and recalls to their respective lists:
+
+for i in thresholds:
+    temp_prediction = [1 if x >= i else 0 for x in y_pred]
+    p = precision(y_true, temp_prediction)
+    r = recall(y_true, temp_prediction)
+    precisions.append(p)
+    recalls.append(r)
+
+plt.figure(figsize=(7, 7))
+plt.plot(recalls, precisions)
+plt.xlabel('Recall', fontsize=15)
+plt.ylabel('Precision', fontsize=15)
+plt.show()
+
+
 
 
 
