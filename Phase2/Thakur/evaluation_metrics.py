47f418bba51d92311223474af5358e9e45921d92
Sampatankar <50807100+Sampatankar@users.noreply.github.com>
2026-02-07 20:57:15 +0000

    final immplementaiton of AP@k

--- Phase2/Thakur/evaluation_metrics.py
+++ Phase2/Thakur/evaluation_metrics.py
@@ -674,5 +674,37 @@ https://github.com/benhammer/Metrics/blobmaster/Python/ml_metrics/average_precis
 
 """
 
+def apk(actual, predicted, k=10):
+    """
+    Computers the average precision at k.  This function computes the AP at k between two lists of items.
+
+    Parameters:
+    actual: list - a list of elements to be predicted (order doesn't matter)
+    predicted: list - a list of predicted elements (order does matter)
+    k: int, optional - the maximum number of predicted elements
+    
+    Return:
+    score: double - the average precision at k over the input lists
+    
+    """
+
+    if len(predicted) > k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i,p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i+1.0)
+
+    if not actual:
+        return 0.0
+    
+    return score / min(len(actual), k)
+
+
+
 
 
